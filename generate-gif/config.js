cf97085e08e97513c154bc644e719b24e120d880
Albert Baliński <albertbalinski@MacBook-Pro-Albert.local>
2018-11-24 15:17:21 +0100

    Fetch generate gif port from env variable

--- generate-gif/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  port: '3003',
-  endpoint() {
-    return 'localhost:' + this.port;
-  }
-}
--- generate-gif/index.js
+++ generate-gif/index.js
@@ -3,41 +3,45 @@
 const express = require("express");
 const app = express();
 const exec = require('child_process').exec;
-const config = require('./config');
 
 function randomStringGen(len) {
   let text = "";
   const charset = "abcdefghijklmnopqrstuvwxyz0123456789";
-  for (var i=0; i < len; i++ )
-      text += charset.charAt(Math.floor(Math.random() * charset.length));
+  for (var i = 0; i < len; i++)
+    text += charset.charAt(Math.floor(Math.random() * charset.length));
   return text;
 }
 
-app.get('/', function (req, res) {
-  const number = req.query.number;
+app.get('/generate_gif/:phoneNumber', function (req, res) {
+  const number = req.params.phoneNumber;
   if (!number) {
     res.end('Error: no number provided');
   } else {
     console.log('[NODE]: Creating gif and sending it to number ' + number);
     exec("bash generate.sh " + number + " " + randomStringGen(5), function (err, stdout, stederr) {
-        if (stederr) {
-            console.log('[NODE]: Generating gif stderr: ' + stederr);
-        }
-        if (stdout) {
-            console.log('[NODE]: Generating gif output:');
-            console.log(stdout);
-        }
-        if (err) {
-            console.log('[NODE]: Error in creating gif: ' + err);
+      if (stederr) {
+        console.log('[NODE]: Generating gif stderr: ' + stederr);
+      }
+      if (stdout) {
+        console.log('[NODE]: Generating gif output:');
+        console.log(stdout);
+      }
+      if (err) {
+        console.log('[NODE]: Error in creating gif: ' + err);
 
-        }
-        const success = err ? false : true;
-        console.log('[NODE]: Finished creating gif with success: ' + success);
-        res.send({ success });
-        res.end();
+      }
+      const success = err ? false : true;
+      console.log('[NODE]: Finished creating gif with success: ' + success);
+      res.send({ success });
+      res.end();
     });
-}
+  }
 });
 
-console.log('[GENERATE] Listening on endpoint: ' + config.endpoint());
-app.listen(config.port);
+const port = process.env.GENERATE_GIF_PORT;
+if (!port) {
+  console.log('[GENERATE_GIF] Error: No port provided. Check environment variable: GENERATE_GIF_PORT');
+} else {
+  console.log('[GENERATE_GIF] Listening on port: ' + port);
+  app.listen(port);
+}
