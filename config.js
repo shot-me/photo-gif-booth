182f748db2ae3bdfa88504128771f4543acd00cb
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2018-11-29 10:39:41 +0100

    Add tests for url

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const productionIp = 'http://192.168.99.100:3003/';
-const devIp = 'http://localhost:3003/';
-const isProduction = true;
+const productionIp = 'http://192.168.99.100:3003';
+const devIp = 'http://localhost:3003';
 
 function getBackendIp() {
+  const isProduction = process.env.NODE_ENV === 'production';
   return isProduction ? productionIp : devIp;
 }
 
--- src/components/shot-me/photos.js
+++ src/components/shot-me/photos.js
@@ -1,8 +1,8 @@
 const productionIp = 'http://192.168.99.100:3002/';
 const devIp = 'http://localhost:3002/';
-const isProduction = true;
 
 export function getBackendIp() {
+  const isProduction = process.env.NODE_ENV === 'production';
   return isProduction ? productionIp : devIp;
 }
 
