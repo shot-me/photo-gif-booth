db1c2386d2b5c8e37fc3956a5329d3111882f105
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2018-11-29 08:58:45 +0100

    Add function getBackendIp()

--- config.js
+++ config.js
@@ -1,22 +1,32 @@
-const backendUp = "http://192.168.99.100:3003/";
+const productionIp = 'http://192.168.99.100:3003/';
+const devIp = 'http://localhost:3003/';
+const isProduction = true;
+
+const getBackendIp = (ip = () => {
+  if (isProduction) {
+    return productionIp;
+  } else {
+    devIp;
+  }
+});
 
 const generateGifConfig = {
   getUrl(number) {
-    return backendUp + "/generate_gif/" + number;
+    return getBackendIp + '/generate_gif/' + number;
   }
 };
 
 const printConfig = {
   port: 3003,
   getUrl(photoName) {
-    return backendUp + "/print/" + photoName;
+    return backendIp + '/print/' + photoName;
   }
 };
 
 const config = {
   print: printConfig,
   generateGif: generateGifConfig,
-  photosDir: "camera_output"
+  photosDir: 'camera_output'
 };
 
 module.exports = config;
