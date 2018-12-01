2440066f1cea62fa0b39de5700f45a3edaf4ad80
Albert Baliński <albert@balin.ski>
2018-12-01 23:59:31 +0100

    Change to 720

--- generate/generate_photo.sh
+++ generate/generate_photo.sh
@@ -9,8 +9,8 @@ fi
 
 echo "[GENERATE PHOTO BASH] Adding branding for file $2 in folder $1"
 
-convert ramka.png -resize 960x480  ramka_small.png
+convert ramka.png -resize 720x480  ramka_small.png
 echo "[GENERATE PHOTO BASH] Frame succesfuly resized"
-convert $1/$2  -resize 960x480  $1/resized.jpg
+convert $1/$2  -resize 720x480  $1/resized.jpg
 composite -geometry +0+0 ramka_small.png $1/resized.jpg $1/print.jpg
 echo "[GENERATE PHOTO BASH] Added branding to frame that will be printed - print.JPG"
