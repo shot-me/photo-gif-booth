88e7a040ff5c8b6f5635c64d295aba648574e7c5
Albert Baliński <albert@balin.ski>
2018-12-01 23:38:46 +0100

    Change ramka pixels to 480

--- generate/generate_photo.sh
+++ generate/generate_photo.sh
@@ -9,7 +9,7 @@ fi
 
 echo "[GENERATE PHOTO BASH] Adding branding for file $2 in folder $1"
 
-convert ramka.png -resize 960x640  ramka_small.png
+convert ramka.png -resize 480x320  ramka_small.png
 echo "[GENERATE PHOTO BASH] Frame succesfuly resized"
 convert $1/$2  -resize 480x320  $1/resized.JPG
 composite -geometry +0+0 ramka_small.png $1/resized.JPG $1/print.JPG
