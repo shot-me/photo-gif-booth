c905e3f0acfb364f777bda453c9c90d519336eed
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2018-12-05 20:21:45 +0100

    Add hardcoded variables

--- server/api.js
+++ server/api.js
@@ -5,8 +5,10 @@ var fs = require('fs'),
     path = require('path');
 
 function getMostRecentFileNames(dir) {
+    if (!dir) {
+        console.log('WEB_APP] Error with get most recent file names. Probably your path to camera output is invalid');
+    }
     var files = fs.readdirSync(dir);
-    console.log(dir);
     return files.filter(fileName => fileName != '.DS_Store').sort(function (a, b) {
         const fullApath = path.join(dir, a);
         const fullBpath = path.join(dir, b);
@@ -17,6 +19,9 @@ function getMostRecentFileNames(dir) {
 /* initRouter create router with endpoint getLatestPhotos.url return list of recent photos */
 function initRouter(config) {
     const { photosDir, nrOfFramesFromCamera } = config;
+    if (!photosDir) {
+        console.log('[ERROR WEB_APP] Cannot initialize express router - invalid path to photos');
+    }
     router.get('/getLatestPhotos', function (req, res) {
         res.send({ photos: getMostRecentFileNames(photosDir).slice(0, nrOfFramesFromCamera) });
         res.end();
