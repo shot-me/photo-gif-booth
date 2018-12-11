5cf0f1a321213ced2a5f2bf9c8a8e598d3947181
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2018-12-11 14:37:50 +0100

    Add test for testing changeing loading state

--- test/enzyme/startScreen.test.js
+++ test/enzyme/startScreen.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import ShotMeStartScreen from './../../src/components/shot-me/sections/ShotMeStartScreen'
 import Adapter from 'enzyme-adapter-react-16';
+import  config from './../config'
 
 Enzyme.configure({adapter: new Adapter()});
+window.config=config;
 
 describe('component: ShotMeStartScreen', () => {
   test('Should check exists of ShotMeStartScreen', ()=>{
@@ -14,8 +16,15 @@ describe('component: ShotMeStartScreen', () => {
   test('Should check two types of intro: loading and lack of internet', ()=> {
     const wrapper = shallow(<ShotMeStartScreen/>);
     const loadingText = wrapper.find('div').children().length;
-    console.log(loadingText);
 
     expect(loadingText).toEqual(2);
+  });
+
+  test('Should check state loading before and after calling checkConnection()', ()=> {
+    const wrapper = shallow(<ShotMeStartScreen/>);
+    const instance = wrapper.instance();
+    expect(wrapper.state().loading).toEqual(true);  // before componentDidMount
+    instance.componentDidMount();
+    setTimeout(()=>expect(wrapper.state().loading).toEqual(false), 1000); // waiting for next interval
   });
 });
