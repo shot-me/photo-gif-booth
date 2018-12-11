f04e29841588c2527880677bd20deba435071d0f
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2018-12-11 08:45:15 +0100

    Add jest config and tests with enzyme

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  coverageReporters: ['json', 'lcov', 'text-summary'],
+  moduleDirectories: ['node_modules', 'app'],
+  moduleNameMapper: {
+    '.*\\.(css|less|styl|scss|sass)$':
+      '<rootDir>/config/jest-mocks/cssModule.js',
+    '.*\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/config/jest-mocks/image.js'
+  },
+  testRegex: 'test/.*\\.test\\.js$'
+};
---
+++ test/enzyme/startScreen.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import expect from 'expect';
+import { shallow } from 'enzyme';
+// import ShotMeStartScreen from './../src/components/shot-me/sections/ShotMeStartScreen'
+
+describe('component: ShotMeStartScreen', () => {
+  it('Links to gif preview screen', ()=>{
+    // const wrapper = shallow(<ShotMeStartScreen/>).length
+    // expect(shallow(<ShotMeStartScreen/>).length).toEqual(4);
+  });
+});
